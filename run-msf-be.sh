87bf3f6ef4448ef6a1bbce16d20ce020c9e69c0f
Steve Zelinka <szelinka@arb.ca.gov>
2020-08-11 14:25:48 -0700

    Switched to CARB RDS

--- run-msf-be.sh
+++ run-msf-be.sh
@@ -3,10 +3,10 @@
 
 HOSTADDR=${1:-0.0.0.0}
 HOSTPORT=${2:-9090}
-PGENDPOINT=${3:-methanesourcefinder.crlexf3r4fgu.us-gov-west-1.rds.amazonaws.com}
+PGENDPOINT=${3:-methane.cluster-cdae8kkz3fpi.us-west-2.rds.amazonaws.com}
 PGPORT=${4:-5432}
 PGUSER=${5:-methane}
-PGPASSWORD=${6:-foo}
+PGPASSWORD=${6:-methaneCH4}
 S3BUCKET=${7:-methane}
 NUMSUBPROCS=${8:-0}
 
